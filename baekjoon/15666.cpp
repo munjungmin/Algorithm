74f3c2b562e230268c2127b4229a19467369569a
munjungmin <answjdals77@gmail.com>
2024-05-26 17:31:34 +0900

    백트래킹 - N과 M (12)

---
+++ baekjoon/15666.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[8]{};
+int seq[8]{};
+
+void dfs(int level) {
+    
+    if(level == M) {
+        for(int i = 0; i < M; i++) {
+            cout << seq[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    int compare = 0;
+    for(int i = 0; i < N; i++) {
+
+        if(level != 0 && arr[i] < seq[level - 1]) continue;
+        if(compare == arr[i]) continue;
+        compare = arr[i];
+        
+        seq[level] = arr[i];
+        dfs(level+1);
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    
+    for(int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    
+    dfs(0);
+    return 0;
+}
+
+
