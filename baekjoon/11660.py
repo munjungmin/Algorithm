bac5b83a806726ed56fab8251fd3eba3ebc89f05
munjungmin <answjdals77@gmail.com>
2026-02-12 15:48:20 +0900

    [11660] 구간 합 구하기 5

---
+++ baekjoon/11660.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+prefix = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+for y in range(n):
+    for x in range(n):
+        prefix[y+1][x+1] = prefix[y][x+1] + prefix[y+1][x] - prefix[y][x] + arr[y][x]
+
+for _ in range(m):
+    y1, x1, y2, x2 = map(int, input().split())
+    answer = prefix[y2][x2] - prefix[y1-1][x2] - prefix[y2][x1-1] + prefix[y1-1][x1-1]
+    print(answer)
