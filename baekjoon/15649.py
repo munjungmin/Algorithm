9d80ad0addc20ee03abe1d4a71741e8de8a83231
munjungmin <answjdals77@gmail.com>
2026-02-18 22:55:34 +0900

    [15649] N과 M (1)

---
+++ baekjoon/15649.py
@@ -0,0 +1,21 @@
+
+
+arr = []
+
+def recur(number):
+    if number == m:
+        print(*arr)
+        return 
+    
+    for i in range(1, n+1):
+        if i in arr : 
+            continue
+        
+        arr.append(i)
+        recur(number + 1)
+        arr.pop()
+
+
+n, m = map(int, input().split())
+
+recur(0)
